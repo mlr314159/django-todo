5f8220bc19d43a41c4ee8600215a20c742867855
Mike <mlr314159@yahoo.co.uk>
2020-04-29 18:24:06 +0000

    edit items added

--- django_todo/todo/views.py
+++ django_todo/todo/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello World")
-
 def get_todo_list(request):
     results = Item.objects.all()
     return render(request, "todo_list.html", {'items': results})
@@ -13,12 +11,25 @@ def get_todo_list(request):
 
 def create_an_item(request):
     if request.method == "POST":
-        new_item = Item()
-        new_item.name = request.POST.get("name")
-        new_item.done = "done" in request.POST
-        new_item.save()
-        
-        return redirect(get_todo_list)
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm()
 
-    return render(request, "item_form.html")
+    return render(request, "item_form.html", {'form': form})
 
+
+def edit_an_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+
+    if request.method == "POST":
+        form  = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm(instance=item)
+
+        return render(request, "item_form.html", {'form': form})
